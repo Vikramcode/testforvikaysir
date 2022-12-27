7fc01a19f5a9851d6cd1c94953428a6eafe4c000
Vikramcode <114869516+Vikramcode@users.noreply.github.com>
2022-12-27 19:10:53 +0530

    Add files via upload

---
+++ formevent.js
@@ -0,0 +1,11 @@
+const namee = document.getElementById("name")
+const namebtnn = document.getElementById("namebtn")
+const submit = document.getElementById("submit")
+
+namebtnn.addEventListener("click" , function(){
+    namee.focus()
+    
+})
+
+
+
